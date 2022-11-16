8237b8675b006f3aae133ece95e0e1aa7983594b
GeorgeAntiado <george.antiado@ucalgary.ca>
2022-11-15 18:21:31 -0700

    Completed the basis for the GUI

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,20 +1,31 @@
 package application;
 	
+import java.io.FileInputStream;
+
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.VBox;
 
 
 public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			BorderPane root = new BorderPane();
-			Scene scene = new Scene(root,400,400);
+
+			FXMLLoader loader = new FXMLLoader();
+			VBox root = loader.load(new FileInputStream("src/application/WorkoutPlanView1.fxml"));
+			Scene scene = new Scene(root,650,450);
+			
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			
+			primaryStage.setTitle("My Workout Plan");
 			primaryStage.setScene(scene);
 			primaryStage.show();
+			
 		} catch(Exception e) {
 			e.printStackTrace();
 		}
---
+++ src/application/WorkoutplanController.java
@@ -0,0 +1,60 @@
+package application;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+public class WorkoutplanController {
+	
+	//Control for the  change in scenes
+	private Stage stage;
+	private Scene scene;
+	
+
+	//This method is used to change into the "Main Menu" Scene
+	public void switchToMainMenu(ActionEvent event) throws IOException {
+		FXMLLoader loader = new FXMLLoader();
+		VBox root = loader.load(new FileInputStream("src/application/WorkoutPlanView1.fxml"));
+		stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+		scene = new Scene(root);
+		stage.setScene(scene);
+		stage.show();
+	}
+	
+	//This method is used to change into the "Calorie Calculator" Scene
+	public void switchToCalorieCalculatorScene(ActionEvent event) throws IOException{
+		FXMLLoader loader = new FXMLLoader();
+		VBox root = loader.load(new FileInputStream("src/application/CalorieCalculatorView.fxml"));
+		stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+		scene = new Scene(root);
+		stage.setScene(scene);
+		stage.show();
+	}
+	
+	//This method is used to change into the "Weekly Eating Plan" Scene
+	public void switchToWeeklyEatingPlan(ActionEvent event) throws IOException{
+		FXMLLoader loader = new FXMLLoader();
+		VBox root = loader.load(new FileInputStream("src/application/WeeklyEatingPlanView.fxml"));
+		stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+		scene = new Scene(root);
+		stage.setScene(scene);
+		stage.show();
+		
+	}
+	
+	//This method is used to change into the "Work out Tracker" Scene
+	public void switchToWeeklyWorkoutTracker(ActionEvent event) throws IOException{
+		FXMLLoader loader = new FXMLLoader();
+		VBox root = loader.load(new FileInputStream("src/application/WorkoutTracker.fxml"));
+		stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+		scene = new Scene(root);
+		stage.setScene(scene);
+		stage.show();
+		}
+	}
