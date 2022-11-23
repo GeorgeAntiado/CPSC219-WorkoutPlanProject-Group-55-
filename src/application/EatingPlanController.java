d45448ecefe6de7899fa27c6594456e3872616a2
luize <luize@10.13.86.5>
2022-11-23 16:35:49 -0700

    first test edit

--- src/application/EatingPlanController.java
+++ src/application/EatingPlanController.java
@@ -1,4 +1,12 @@
 package application;
+import javafx.scene.control.*;
+import javafx.scene.layout.*;
+
+import java.util.ArrayList;
+
+import javafx.collections.FXCollections;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 
 public class EatingPlanController {
 	
