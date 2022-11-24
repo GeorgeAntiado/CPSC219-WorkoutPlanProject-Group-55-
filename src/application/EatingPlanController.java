10ab6878e87189c1c6bbd477946a193f8b6c9a64
luize <luize@10.13.102.159>
2022-11-24 09:50:20 -0700

    minor edit

--- src/application/EatingPlanController.java
+++ src/application/EatingPlanController.java
@@ -10,4 +10,73 @@ import javafx.fxml.FXML;
 
 public class EatingPlanController {
 	
+	@FXML
+	ChoiceBox<Integer> mondaymeals;
+	
+	@FXML
+	ChoiceBox<Integer> tuesdaymeals;
+	
+	@FXML
+	ChoiceBox<Integer> wednesdaymeals;
+	
+	@FXML
+	ChoiceBox<Integer> thursdaymeals;
+	
+	@FXML
+	ChoiceBox<Integer> fridaymeals;
+	
+	@FXML
+	ChoiceBox<Integer> saturdaymeals;
+	
+	@FXML
+	ChoiceBox<Integer> sundaymeals;
+	
+	@FXML
+	ChoiceBox<Integer> mondayworkouts;
+	
+	@FXML
+	ChoiceBox<Integer> tuesdayworkouts;
+	
+	@FXML
+	ChoiceBox<Integer> wednesdayworkouts;
+	
+	@FXML
+	ChoiceBox<Integer> thursdayworkouts;
+	
+	@FXML
+	ChoiceBox<Integer> fridayworkouts;
+	
+	@FXML
+	ChoiceBox<Integer> saturdayworkouts;
+	
+	@FXML
+	ChoiceBox<Integer> sundayworkouts;
+	
+	@FXML
+	Label monCalCount;
+	
+	@FXML
+	Label tuesCalCount;
+	
+	@FXML
+	Label wedCalCount;
+	
+	@FXML
+	Label thursCalCount;
+	
+	@FXML
+	Label friCalCount;
+	
+	@FXML
+	Label satCalCount;
+	
+	@FXML
+	Label sunCalCount;
+	
+	int dailyCalorieIntake = 2200;
+	
+	@FXML 
+	void someEvent(ActionEvent something) {
+		
+	}
 }
