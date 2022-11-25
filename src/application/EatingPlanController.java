a579d50b2aa6cd6e4b2d2f87218308d2704b27c4
luize <luize@FORREST-GUMP>
2022-11-24 23:01:23 -0700

    adding exercise class

--- src/application/EatingPlanController.java
+++ src/application/EatingPlanController.java
@@ -1,5 +1,85 @@
 package application;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.layout.*;
+import javafx.scene.control.*;
+
 public class EatingPlanController {
 	
+	@FXML
+	ChoiceBox<Integer> mondaymeals;
+	
+	@FXML
+	ChoiceBox<Integer> tuesdaymeals;
+	
+	@FXML
+	ChoiceBox<Integer> wednesdaymeals;
+	
+	@FXML
+	ChoiceBox<Integer> thursdaymeals;
+	
+	@FXML
+	ChoiceBox<Integer> fridaymeals;
+	
+	@FXML
+	ChoiceBox<Integer> saturdaymeals;
+	
+	@FXML
+	ChoiceBox<Integer> sundaymeals;
+	
+	@FXML
+	ChoiceBox<Integer> mondayworkouts;
+	
+	@FXML
+	ChoiceBox<Integer> tuesdayworkouts;
+	
+	@FXML
+	ChoiceBox<Integer> wednesdayworkouts;
+	
+	@FXML
+	ChoiceBox<Integer> thursdayworkouts;
+	
+	@FXML
+	ChoiceBox<Integer> fridayworkouts;
+	
+	@FXML
+	ChoiceBox<Integer> saturdayworkouts;
+	
+	@FXML
+	ChoiceBox<Integer> sundayworkouts;
+	
+	@FXML
+	Label monCalCount;
+	
+	@FXML
+	Label tuesCalCount;
+	
+	@FXML
+	Label wedCalCount;
+	
+	@FXML
+	Label thursCalCount;
+	
+	@FXML
+	Label friCalCount;
+	
+	@FXML
+	Label satCalCount;
+	
+	@FXML
+	Label sunCalCount;
+	
+	int dailyCalorieIntake = 2200;
+	
+	@FXML
+	void setDailyCalorie(ActionEvent calorieEvent) {
+		monCalCount.setText(dailyCalorieIntake + "to go!");
+		tuesCalCount.setText(dailyCalorieIntake + "to go!");
+		wedCalCount.setText(dailyCalorieIntake + "to go!");
+		thursCalCount.setText(dailyCalorieIntake + "to go!");
+		friCalCount.setText(dailyCalorieIntake + "to go!");
+		satCalCount.setText(dailyCalorieIntake + "to go!");
+		sunCalCount.setText(dailyCalorieIntake + "to go!");
+	}
 }
---
+++ src/application/exercise.java
@@ -0,0 +1,54 @@
+package application;
+
+public class exercise {
+	
+	//attributes
+	String exerciseType;
+	int exerciseLength;
+	String intensity;
+	int caloriesBurnt = 0;
+		
+	public exercise(String typeOfExercise, String intensityOfExercise, int lengthOfExercise) {
+		exerciseType = typeOfExercise;
+		intensity = intensityOfExercise;
+		exerciseLength = lengthOfExercise;
+	}
+	int getcaloriesBurnt() {
+		if (exerciseType == "cardio") {
+			if (intensity == "light") {
+				caloriesBurnt = exerciseLength * 4;
+			}
+			else if (intensity=="medium") {
+				caloriesBurnt = exerciseLength * 12;
+			}
+			else if (intensity == "hard") {
+				caloriesBurnt = exerciseLength * 30;
+			}	
+		}
+		else if (exerciseType == "WeightLifitng") {
+			if (intensity == "light") {
+				caloriesBurnt = exerciseLength * 3;
+			}
+			else if(intensity == "medium") {
+				caloriesBurnt = exerciseLength * 5;
+			}
+			else if (intensity =="hard") {
+				caloriesBurnt = exerciseLength * 10;
+			}
+		}
+		else if (exerciseType =="sports") {
+			if (intensity == "light") {
+				caloriesBurnt = exerciseLength * 5;
+			}
+			else if(intensity == "medium") {
+				caloriesBurnt = exerciseLength * 9;
+			}
+			else if (intensity =="hard") {
+				caloriesBurnt = exerciseLength * 13;
+			}
+		}
+		return caloriesBurnt;
+	}
+	
+
+}
