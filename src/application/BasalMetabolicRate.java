9d9cd218b8f7b3f74416c633d5f08e6757cd9343
Sage Sieppert <lauren.sieppert@ucalgary.ca>
2022-11-30 18:41:33 -0700

    Finished constructor for BasalMetabolicRate

--- src/application/BasalMetabolicRate.java
+++ src/application/BasalMetabolicRate.java
@@ -13,11 +13,79 @@ public class BasalMetabolicRate {
 		age = uAge;
 	}
 	
-	BasalMetabolicRate(String uWeight, String uHeight, String uAge, String uSex){
-		sex = uSex;
-		//weight = uWeight;
-		//height = uHeight;
-		//age = uAge;
+	BasalMetabolicRate(String uWeight, String uHeight, String uAge, String uSex) throws InvalidNumberException{
+		sex = uSex; // no need to validate because it is a choice box
+		
+		
+		boolean validNumbers = true;
+		
+		// validates that uAge is an integer
+		for (char i : uAge.toCharArray()) {
+    		// Check if character is a digit. 
+    		if (!Character.isDigit(i)) {
+    			// Check if character is a decimal.
+    			if (i == '.') {
+    				validNumbers = false;
+    				throw new InvalidNumberException("Don't use decimals in your age.");
+    			} else {
+    				validNumbers = false;
+    				throw new InvalidNumberException("Don't use " + i + " in your age. Make sure to enter a number.");
+    			}
+    		}	
+		}
+		
+		
+		boolean checkForDecimal = false;
+		for (char i : uWeight.toCharArray()) {
+    		// Check if character is a digit. 
+    		if (!Character.isDigit(i)) {
+    			// Check if character is a decimal.
+    			if (i == '.') {
+    				// Check if there's already a decimal, output an error if so. If there isn't, know that there is one decimal now.
+    				if (checkForDecimal) {
+    					validNumbers = false;
+    					throw new InvalidNumberException("Don't use more than one " + i + " in your weight");
+    				} else {
+    					checkForDecimal = true;
+    				}
+    			} else {
+    				validNumbers = false;
+    				throw new InvalidNumberException("Don't use " + i + " in your weight. Make sure to enter a number.");
+    			}
+    		}	
+		}
+		
+		
+		checkForDecimal = false;
+		for (char i : uHeight.toCharArray()) {
+    		// Check if character is a digit. 
+    		if (!Character.isDigit(i)) {
+    			// Check if character is a decimal.
+    			if (i == '.') {
+    				// Check if there's already a decimal, output an error if so. If there isn't, know that there is one decimal now.
+    				if (checkForDecimal) {
+    					validNumbers = false;
+    					throw new InvalidNumberException("Don't use more than one " + i + " in your height.");
+    				} else {
+    					checkForDecimal = true;
+    				}
+    			} else {
+    				validNumbers = false;
+    				throw new InvalidNumberException("Don't use " + i + " in your height. Make sure to enter a number.");
+    			}
+    		}	
+		}
+		
+		if (validNumbers) {
+		weight = Double.parseDouble(uWeight);
+		height = Double.parseDouble(uHeight);
+		age = Integer.parseInt(uAge);
+		}
+		
+		if(weight < 0 || height < 0 || age < 0) {
+			throw new InvalidNumberException("You entered a negative number. Please make sure all numbers are greater than 0.");
+		}
+		
 	}
 	
 }
