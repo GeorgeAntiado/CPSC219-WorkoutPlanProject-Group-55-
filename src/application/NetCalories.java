74c2d0b5efccb86a01ec584369120178af0d2cff
Sage Sieppert <lauren.sieppert@ucalgary.ca>
2022-12-08 22:00:49 -0700

    Javadoc for NetCalories done.

--- src/application/NetCalories.java
+++ src/application/NetCalories.java
@@ -3,23 +3,43 @@ package application;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 
+/**
+ * Used by setCalCountLabel() in EatingPlanController
+ * @author CS219-user
+ */
 public class NetCalories {
-int calorieMaintenance;
-int caloriesBurnt;
-int caloriesEaten;
-int netCalories;
-
-public NetCalories(int cMaintenance, int cBurnt, int cEaten) {
-	calorieMaintenance = cMaintenance;
-	caloriesBurnt = cBurnt;
-	caloriesEaten = cEaten;
-}
-public int calcNetCalories() {
-	netCalories = calorieMaintenance+caloriesBurnt-caloriesEaten;
-	return netCalories;
-}
-//Sets the Labels for each day to how many calories the user needs to eat after inputting the amount 
-	// of calories burnt from exercising and calories eaten from the different meals throughout the day
+	int calorieMaintenance;
+	int caloriesBurnt;
+	int caloriesEaten;
+	int netCalories;
+	
+	/**
+	 * Constructor for NetCalories.
+	 * @param cMaintenance
+	 * @param cBurnt
+	 * @param cEaten
+	 */
+	public NetCalories(int cMaintenance, int cBurnt, int cEaten) {
+		calorieMaintenance = cMaintenance;
+		caloriesBurnt = cBurnt;
+		caloriesEaten = cEaten;
+	}
+	
+	/**
+	 * Calculates the calories based on the instance's variables.
+	 * @return netCalories: int
+	 */
+	public int calcNetCalories() {
+		netCalories = calorieMaintenance+caloriesBurnt-caloriesEaten;
+		return netCalories;
+	}
+	
+	
+	/**
+	 * Sets the Labels for how many calories the user needs to eat based on netCalories.
+	 * Should be used after calcNetCalories sets the netCalories.
+	 * @return requiredCalories: String
+	 */
 	public String setCalorieLabel() {
 		
 		String requiredCalories = "";
